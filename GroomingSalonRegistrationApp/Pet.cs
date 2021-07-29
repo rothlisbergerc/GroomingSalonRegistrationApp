eb9b25fb73457de307e5c1a6826fdb29a556f6a3
Ryder <rdeback@gmail.com>
2021-07-28 21:27:29 -0700

    Created starter pet class

---
+++ GroomingSalonRegistrationApp/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroomingSalonRegistrationApp
+{
+    class Pet
+    {
+        public int PetId { get; set; }
+
+        public String Name { get; set; }
+
+        public String Breed { get; set; }
+
+        //Made a float because most animals won't live past roughly 25-30 
+        public float Age { get; set; }
+    }
+}
