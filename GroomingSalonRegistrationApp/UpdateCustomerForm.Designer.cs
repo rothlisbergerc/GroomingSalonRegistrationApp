8e026b09a6f3035a5b856132afe797a38f1ca7cd
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-30 11:39:38 -0700

    Assigned the form's accept button to the Submit (updateCustSubmitBtn) button

--- GroomingSalonRegistrationApp/UpdateCustomerForm.Designer.cs
+++ GroomingSalonRegistrationApp/UpdateCustomerForm.Designer.cs
@@ -125,6 +125,7 @@ namespace GroomingSalonRegistrationApp
             // 
             // UpdateCustomerForm
             // 
+            this.AcceptButton = this.updateCustSubmitBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(445, 201);
