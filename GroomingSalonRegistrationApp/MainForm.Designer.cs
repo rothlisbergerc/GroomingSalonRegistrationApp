890b1589a29796c7410b53a030ba1a20d001c5d6
RyderDProgrammer <77074811+RyderDProgrammer@users.noreply.github.com>
2021-07-30 13:25:57 -0700

    Issue#05 implement create (#39)
    * Created add in SalonDb
    
    * Made add customer and add pet open respective pages.
    
    * Got add customer working
    
    * Add fully working

--- GroomingSalonRegistrationApp/MainForm.Designer.cs
+++ GroomingSalonRegistrationApp/MainForm.Designer.cs
@@ -81,6 +81,7 @@ namespace GroomingSalonRegistrationApp
             this.addCustBtn.TabIndex = 4;
             this.addCustBtn.Text = "Add Customer";
             this.addCustBtn.UseVisualStyleBackColor = true;
+            this.addCustBtn.Click += new System.EventHandler(this.addCustBtn_Click);
             // 
             // addPetBtn
             // 
@@ -90,6 +91,7 @@ namespace GroomingSalonRegistrationApp
             this.addPetBtn.TabIndex = 5;
             this.addPetBtn.Text = "Add Pet";
             this.addPetBtn.UseVisualStyleBackColor = true;
+            this.addPetBtn.Click += new System.EventHandler(this.addPetBtn_Click);
             // 
             // updateBtn
             // 
