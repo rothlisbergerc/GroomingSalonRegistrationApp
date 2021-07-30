aca1fe9f7015368c41bb9aefd74048f2db372388
Ryder <rdeback@gmail.com>
2021-07-30 13:23:54 -0700

    Add fully working

--- GroomingSalonRegistrationApp/MainForm.cs
+++ GroomingSalonRegistrationApp/MainForm.cs
@@ -20,14 +20,25 @@ namespace GroomingSalonRegistrationApp
         private void MainForm_Load(object sender, EventArgs e)
         {
             List<Customer> allCustomers = SalonDb.getAllCustomers();
+            custList(allCustomers);
+        }
 
-            custListBox.DataSource = allCustomers;
+        private void custList(List<Customer> custs)
+        {
+            custListBox.Items.Clear();
+            foreach(Customer c in custs)
+            {
+                custListBox.Items.Add(c);
+            }
         }
 
         private void addCustBtn_Click(object sender, EventArgs e)
         {
             AddCustomerForm newCust = new AddCustomerForm();
             newCust.ShowDialog();
+
+            List<Customer> allCustomers = SalonDb.getAllCustomers();
+            custList(allCustomers);
         }
 
         private void addPetBtn_Click(object sender, EventArgs e)
