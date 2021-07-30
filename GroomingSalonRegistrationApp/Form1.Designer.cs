2d5d6b4be10d1affeafbe1a7d2aa476b6bcbcb8e
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-30 09:23:44 -0700

    Gave all the controls in Form1 names and text.

--- GroomingSalonRegistrationApp/Form1.Designer.cs
+++ GroomingSalonRegistrationApp/Form1.Designer.cs
@@ -29,99 +29,99 @@ namespace GroomingSalonRegistrationApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.custListBox = new System.Windows.Forms.ListBox();
+            this.petComboBox = new System.Windows.Forms.ComboBox();
+            this.custListBoxLabel = new System.Windows.Forms.Label();
+            this.petCboLabel = new System.Windows.Forms.Label();
+            this.addCustBtn = new System.Windows.Forms.Button();
+            this.addPetBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // custListBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(81, 33);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(167, 251);
-            this.listBox1.TabIndex = 0;
+            this.custListBox.FormattingEnabled = true;
+            this.custListBox.Location = new System.Drawing.Point(81, 33);
+            this.custListBox.Name = "custListBox";
+            this.custListBox.Size = new System.Drawing.Size(167, 251);
+            this.custListBox.TabIndex = 0;
             // 
-            // comboBox1
+            // petComboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(328, 33);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(235, 21);
-            this.comboBox1.TabIndex = 1;
+            this.petComboBox.FormattingEnabled = true;
+            this.petComboBox.Location = new System.Drawing.Point(328, 33);
+            this.petComboBox.Name = "petComboBox";
+            this.petComboBox.Size = new System.Drawing.Size(235, 21);
+            this.petComboBox.TabIndex = 1;
             // 
-            // label1
+            // custListBoxLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.custListBoxLabel.AutoSize = true;
+            this.custListBoxLabel.Location = new System.Drawing.Point(27, 33);
+            this.custListBoxLabel.Name = "custListBoxLabel";
+            this.custListBoxLabel.Size = new System.Drawing.Size(51, 13);
+            this.custListBoxLabel.TabIndex = 2;
+            this.custListBoxLabel.Text = "Customer";
             // 
-            // label2
+            // petCboLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(274, 33);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.petCboLabel.AutoSize = true;
+            this.petCboLabel.Location = new System.Drawing.Point(274, 33);
+            this.petCboLabel.Name = "petCboLabel";
+            this.petCboLabel.Size = new System.Drawing.Size(23, 13);
+            this.petCboLabel.TabIndex = 3;
+            this.petCboLabel.Text = "Pet";
             // 
-            // button1
+            // addCustBtn
             // 
-            this.button1.Location = new System.Drawing.Point(592, 33);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 51);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.addCustBtn.Location = new System.Drawing.Point(592, 33);
+            this.addCustBtn.Name = "addCustBtn";
+            this.addCustBtn.Size = new System.Drawing.Size(138, 51);
+            this.addCustBtn.TabIndex = 4;
+            this.addCustBtn.Text = "Add Customer";
+            this.addCustBtn.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // addPetBtn
             // 
-            this.button2.Location = new System.Drawing.Point(592, 112);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 51);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.addPetBtn.Location = new System.Drawing.Point(592, 112);
+            this.addPetBtn.Name = "addPetBtn";
+            this.addPetBtn.Size = new System.Drawing.Size(138, 51);
+            this.addPetBtn.TabIndex = 5;
+            this.addPetBtn.Text = "Add Pet";
+            this.addPetBtn.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // updateBtn
             // 
-            this.button3.Location = new System.Drawing.Point(592, 197);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(138, 51);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.updateBtn.Location = new System.Drawing.Point(592, 197);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(138, 51);
+            this.updateBtn.TabIndex = 6;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // deleteBtn
             // 
-            this.button4.Location = new System.Drawing.Point(592, 277);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(138, 51);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.deleteBtn.Location = new System.Drawing.Point(592, 277);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(138, 51);
+            this.deleteBtn.TabIndex = 7;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.addPetBtn);
+            this.Controls.Add(this.addCustBtn);
+            this.Controls.Add(this.petCboLabel);
+            this.Controls.Add(this.custListBoxLabel);
+            this.Controls.Add(this.petComboBox);
+            this.Controls.Add(this.custListBox);
             this.Name = "Form1";
             this.Text = "Grooming Salon Registration";
             this.ResumeLayout(false);
@@ -131,14 +131,14 @@ namespace GroomingSalonRegistrationApp
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox custListBox;
+        private System.Windows.Forms.ComboBox petComboBox;
+        private System.Windows.Forms.Label custListBoxLabel;
+        private System.Windows.Forms.Label petCboLabel;
+        private System.Windows.Forms.Button addCustBtn;
+        private System.Windows.Forms.Button addPetBtn;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button deleteBtn;
     }
 }
 
