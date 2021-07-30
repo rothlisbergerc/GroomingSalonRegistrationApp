ccc7f17dea64dafba50ea0342d7e3e0b93905b45
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-30 12:01:13 -0700

    Named the control elements and added text as appropriate.

--- GroomingSalonRegistrationApp/UpdatePetForm.Designer.cs
+++ GroomingSalonRegistrationApp/UpdatePetForm.Designer.cs
@@ -29,60 +29,60 @@ namespace GroomingSalonRegistrationApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.updatePetSubmitBtn = new System.Windows.Forms.Button();
+            this.updatePetCancelBtn = new System.Windows.Forms.Button();
+            this.updatePetNameLabel = new System.Windows.Forms.Label();
+            this.updatePetBreedLabel = new System.Windows.Forms.Label();
+            this.updatePetAgeLabel = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // updatePetSubmitBtn
             // 
-            this.button1.Location = new System.Drawing.Point(184, 146);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.updatePetSubmitBtn.Location = new System.Drawing.Point(184, 146);
+            this.updatePetSubmitBtn.Name = "updatePetSubmitBtn";
+            this.updatePetSubmitBtn.Size = new System.Drawing.Size(75, 23);
+            this.updatePetSubmitBtn.TabIndex = 0;
+            this.updatePetSubmitBtn.Text = "Submit";
+            this.updatePetSubmitBtn.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // updatePetCancelBtn
             // 
-            this.button2.Location = new System.Drawing.Point(319, 146);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.updatePetCancelBtn.Location = new System.Drawing.Point(319, 146);
+            this.updatePetCancelBtn.Name = "updatePetCancelBtn";
+            this.updatePetCancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.updatePetCancelBtn.TabIndex = 1;
+            this.updatePetCancelBtn.Text = "Cancel";
+            this.updatePetCancelBtn.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // updatePetNameLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 59);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.updatePetNameLabel.AutoSize = true;
+            this.updatePetNameLabel.Location = new System.Drawing.Point(45, 59);
+            this.updatePetNameLabel.Name = "updatePetNameLabel";
+            this.updatePetNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.updatePetNameLabel.TabIndex = 2;
+            this.updatePetNameLabel.Text = "Name";
             // 
-            // label2
+            // updatePetBreedLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 85);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.updatePetBreedLabel.AutoSize = true;
+            this.updatePetBreedLabel.Location = new System.Drawing.Point(45, 85);
+            this.updatePetBreedLabel.Name = "updatePetBreedLabel";
+            this.updatePetBreedLabel.Size = new System.Drawing.Size(35, 13);
+            this.updatePetBreedLabel.TabIndex = 3;
+            this.updatePetBreedLabel.Text = "Breed";
             // 
-            // label3
+            // updatePetAgeLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 111);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.updatePetAgeLabel.AutoSize = true;
+            this.updatePetAgeLabel.Location = new System.Drawing.Point(45, 111);
+            this.updatePetAgeLabel.Name = "updatePetAgeLabel";
+            this.updatePetAgeLabel.Size = new System.Drawing.Size(26, 13);
+            this.updatePetAgeLabel.TabIndex = 4;
+            this.updatePetAgeLabel.Text = "Age";
             // 
             // textBox1
             // 
@@ -113,11 +113,11 @@ namespace GroomingSalonRegistrationApp
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.updatePetAgeLabel);
+            this.Controls.Add(this.updatePetBreedLabel);
+            this.Controls.Add(this.updatePetNameLabel);
+            this.Controls.Add(this.updatePetCancelBtn);
+            this.Controls.Add(this.updatePetSubmitBtn);
             this.Name = "UpdatePetForm";
             this.Text = "Update Pet";
             this.ResumeLayout(false);
@@ -127,11 +127,11 @@ namespace GroomingSalonRegistrationApp
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button updatePetSubmitBtn;
+        private System.Windows.Forms.Button updatePetCancelBtn;
+        private System.Windows.Forms.Label updatePetNameLabel;
+        private System.Windows.Forms.Label updatePetBreedLabel;
+        private System.Windows.Forms.Label updatePetAgeLabel;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
