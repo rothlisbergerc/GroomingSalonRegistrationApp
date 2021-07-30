07b9fec508c58b6a2336b6f67a67c0dc9e4e7add
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-07-30 11:33:16 -0700

    Added and arranged control elements

--- GroomingSalonRegistrationApp/UpdateCustomerForm.Designer.cs
+++ GroomingSalonRegistrationApp/UpdateCustomerForm.Designer.cs
@@ -31,36 +31,117 @@ namespace GroomingSalonRegistrationApp
         {
             this.updateCustSubmitBtn = new System.Windows.Forms.Button();
             this.updateCustCancelBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // updateCustSubmitBtn
             // 
-            this.updateCustSubmitBtn.Location = new System.Drawing.Point(270, 158);
+            this.updateCustSubmitBtn.Location = new System.Drawing.Point(155, 139);
             this.updateCustSubmitBtn.Name = "updateCustSubmitBtn";
-            this.updateCustSubmitBtn.Size = new System.Drawing.Size(75, 23);
+            this.updateCustSubmitBtn.Size = new System.Drawing.Size(105, 32);
             this.updateCustSubmitBtn.TabIndex = 0;
             this.updateCustSubmitBtn.Text = "Submit";
             this.updateCustSubmitBtn.UseVisualStyleBackColor = true;
             // 
             // updateCustCancelBtn
             // 
-            this.updateCustCancelBtn.Location = new System.Drawing.Point(374, 158);
+            this.updateCustCancelBtn.Location = new System.Drawing.Point(303, 139);
             this.updateCustCancelBtn.Name = "updateCustCancelBtn";
-            this.updateCustCancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.updateCustCancelBtn.Size = new System.Drawing.Size(105, 32);
             this.updateCustCancelBtn.TabIndex = 1;
             this.updateCustCancelBtn.Text = "Cancel";
             this.updateCustCancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(253, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(253, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(155, 101);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(253, 20);
+            this.textBox4.TabIndex = 9;
             // 
             // UpdateCustomerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(474, 216);
+            this.ClientSize = new System.Drawing.Size(459, 201);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.updateCustCancelBtn);
             this.Controls.Add(this.updateCustSubmitBtn);
             this.Name = "UpdateCustomerForm";
             this.Text = "Update Customer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,5 +149,13 @@ namespace GroomingSalonRegistrationApp
 
         private System.Windows.Forms.Button updateCustSubmitBtn;
         private System.Windows.Forms.Button updateCustCancelBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
